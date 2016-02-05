849b3981c21dd878713b89a812ea347a132de58a
Jay Hogan <seeker136@gmail.com>
2016-02-05 05:12:39 -0500

    Fix build

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        This doesn't work
+        //This doesn't work
         SpringApplication.run(Application.class, args);
     }
 }
